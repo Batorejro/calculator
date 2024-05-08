2493f3a96bbd5b95c1e3b9b63d81f83c5370362b
Arkadiusz Pedzisz <serwis.artech@gmail.com>
2024-05-09 01:33:00 +0200

    dodano timer do kodu

--- kalk.py
+++ kalk.py
@@ -1,8 +1,5 @@
 import math
-#  import sys
-import logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
-#filename="kalkfile.log"
+import time
 
 
 def pole_prostokata(a, b):
@@ -14,15 +11,20 @@ def pole_kwadratu(a):
 
 
 def pole_rombu(a, b, h):
-     return (a + b) / 2 * h
+    return (a + b) / 2 * h
 
 
 def pole_koła(r):
-    return math.pi * r** 2
+    return math.pi * r ** 2
 
 
 def pole_trójkata(a, h):
     return 0.5 * a * h
+
+
+def finish_timer(start):
+    end = time.perf_counter()
+    return end-start
 
 
 #  wybór = (input("1 - Pole prostokąta, 2 - Pole kwadratu, 3 - Pole rombu, 4 - Pole pizzy, 5 - Pole trójkąta: "))
@@ -42,32 +44,41 @@ while (True):
     wybór = input("Co chcesz zrobić? ")
 
     if (wybór == '1'):
+        start = time.perf_counter()
         a = int(input("Wprowadż długość boku a: "))
-        b = int(input("Wprowadż długość boku b: "))
+        b = int(input("Wprowadż długość boku b: "))   
         print(pole_prostokata(a, b))
+        print("Czas operacyjny działania: ", finish_timer(start))
     elif (wybór == '2'):
+        start = time.perf_counter()
         a = int(input("Wprowadż długość boku a: "))
+        end = time.perf_counter()
         print(pole_kwadratu(a))
+        print("Czas operacyjny działania: ", finish_timer(start))
     elif (wybór == '3'):
+        start = time.perf_counter()
         a = int(input("Wprowadż długość boku a: "))
         b = int(input("Wprowadż długość boku b: "))
         h = int(input("Wprowadż wysokość h: "))
         print(pole_rombu(a, b, h))
+        print("Czas operacyjny działania: ", finish_timer(start))
+
     elif (wybór == '4'):
+        start = time.perf_counter()
         r = int(input("Wprowadż promień koła r: "))
         print(pole_koła(r))
+        print("Czas operacyjny działania: ", finish_timer(start))
 
     elif (wybór == '5'):
+        start = time.perf_counter()
         a = int(input("Wprowadż długość boku a: "))
         h = int(input("Wprowadż wysokość h: "))
         print(pole_trójkata(a, h))
+        print("Czas operacyjny działania: ", finish_timer(start))
 
-    elif ( wybór > '5'):
+    elif (wybór > '5'):
         print("ucha, ucha, taniec brzucha")
         break
     else:
         print(" HE, He, he, tra la la - ten znak to nie jest liczba!! ")
         break
-
-
-
